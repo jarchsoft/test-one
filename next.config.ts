f27a02a6e037fbd800c4e1327af81b29c87ce3ef
jarchsoft <johnnyarchsoftware@gmail.com>
2025-09-08 11:02:24 -0400

    Test2 GoogleAnalytics

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
      output: 'export',
-      basePath: '/test-one',
+     
+       basePath: process.env.NODE_ENV === 'production' ? '/test-one' : '',
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { GoogleTagManager } from '@next/third-parties/google';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,12 +27,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+          <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-HJDBYKLNMM');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+               <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-HJDBYKLNMM"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         {children}
       </body>
-      <GoogleTagManager gtmId="G-HJDBYKLNMM" />
+      
     </html>
   );
 }
