66e91f01abc90c20322446fd3240962c5cb15a09
jarchsoft <johnnyarchsoftware@gmail.com>
2025-09-08 11:05:10 -0400

    G-code fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-HJDBYKLNMM');`,
+            })(window,document,'script','dataLayer','G-HJDBYKLNMM');`,
           }}
         />
         {/* End Google Tag Manager */}
@@ -45,7 +45,7 @@ export default function RootLayout({
       >
                <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-HJDBYKLNMM"
+            src="https://www.googletagmanager.com/ns.html?id=G-HJDBYKLNMM"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
