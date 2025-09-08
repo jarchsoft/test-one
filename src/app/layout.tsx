6007ef3998aa4e1b34cf84976fa256a49637a2ea
jarchsoft <johnnyarchsoftware@gmail.com>
2025-09-08 10:36:35 -0400

    Tagmanager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+import { GoogleTagManager } from '@next/third-parties/google';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +33,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleTagManager gtmId="G-HJDBYKLNMM" />
     </html>
   );
 }
